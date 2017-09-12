8e414cd3dbe63ed7bf53a3213c384ab053fead27
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2017-09-12 11:46:22 -0400

    codingconventions

---
+++ YonathanBenReuven/Codingconventionsyonathan/src/intro/CodingConventions.java
@@ -0,0 +1,12 @@
+package intro;
+
+public class CodingConventions {
+	private String name;
+	private String description;
+	
+	public CodingConventions() {
+		name = "mrNockles";
+		description = "teacher";
+	}
+
+}
