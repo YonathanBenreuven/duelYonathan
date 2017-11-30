cb805da07c6a7bb9037288a2b5435c24501e3c40
BT_1N3_27 <BT_1N3_27@BT_1N3_27.bktech.local>
2017-11-30 11:45:12 -0500

    food

---
+++ DUELYonathan/src/search/LionsPuzzle.java
@@ -0,0 +1,18 @@
+package search;
+
+public class LionsPuzzle {
+	public static void main(String[] args) {
+		int lions = 100;
+		System.out.println(lions + " lions decide to eat the sheep?" +eatSheepOrNot(lions));
+	}
+
+	private static boolean eatSheepOrNot(int lions) {
+		if(lions ==1) {
+			return true;
+		}else {
+			return !eatSheepOrNot(lions-1);
+		}
+		
+		
+	}
+}
